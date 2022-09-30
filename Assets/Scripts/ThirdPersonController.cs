45c14584073e3928709f24dc0cdacb718487a030
Sydewa <manelramosroyocts2019@gmail.com>
2022-09-30 13:32:08 +0200

    30_9

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -6,6 +6,7 @@ public class ThirdPersonController : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    public Transform cam;
     
     public Transform groundSensor;
     public LayerMask ground;
@@ -17,25 +18,83 @@ public class ThirdPersonController : MonoBehaviour
     public float gravity = -9.81f;
     public bool isGrounded;
 
-    void Awake()
+    //Movimiento suave al rotar
+    private float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+
+    
+
+    void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
     void Update()
     {
+        
+        //Movement();
+        //MovementTPS();
+        MovementTPS2();
+        
+        Jump();
+
+    }
+
+    
+    
+    void Movement()
+    {
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
         if(move != Vector3.zero)
         {
-            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-            controller.Move(move * speed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+    }
 
+    void MovementTPS()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
-        //isGrounded = controller.isGrounded;
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    
+    //movimiento tps con virtual camara
+    void MovementTPS2()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    
+    
+    void Jump()
+    {
         isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
         if(isGrounded && playerVelocity.y < 0)
         {
@@ -52,8 +111,6 @@ public class ThirdPersonController : MonoBehaviour
 
         playerVelocity.y += gravity * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
     }
-    //pls dear god make this code work, padre nuestro que estas en los cielos sea santificado tu nombre venga a nosotros tu voluntead asi Padre nuestro, que estás en el cielo, santificado sea tu Nombre; venga a nosotros tu reino; hágase tu voluntad en la tierra como en el cielo.
- //Danos hoy nuestro pan de cada día; perdona nuestras ofensas como también nosotros perdonamos a los que nos ofenden; no nos dejes caer en la tentación y líbranos del mal. Amén.
+    
 }
